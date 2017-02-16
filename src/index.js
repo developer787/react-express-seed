91909a1fcb9f9e24e6e0fd4e5852352805fe04c6
Nodejs Puerto Rico <nodejs787@gmail.com>
2017-02-16 17:48:56 +0000

    Toggle on/off button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Toggle from './toggle';
+
 import './index.css';
 
 ReactDOM.render(
   <App />,
+  <Toggle />,
   document.getElementById('root')
 );
